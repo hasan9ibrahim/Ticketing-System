ac9cbfbf7691cdebc654af9387c3d0a521cbc4a6
Hasan <87708064+hasan9ibrahim@users.noreply.github.com>
2026-02-15 16:51:57 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -872,8 +872,9 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["*"],
     allow_methods=["*"],
+    allow_headers=["*"],
     allow_headers=["*"],
 )
 
